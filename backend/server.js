73affe7e3c3cf0f6dac2cf2f423d32790d25109b
oliver <oliverlsanz@gmail.com>
2020-05-01 11:27:19 +0200

    Connect to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 // check for dependecies
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');  // to connect to MongoDB
 
 // check for dotenv dependency and configure it
-require('dotenv').config();
+require('dotenv').config();  // to easily set up env variables
 
 // create the express server
 const app = express();
@@ -12,6 +13,15 @@ const port = process.env.PORT || 5000;
 // set up middleware
 app.use(cors());
 app.use(express.json());
+
+// conect with MongoDB Atlas
+const uri = process.env.ATLAS_URI;  // ATLAS_URI is found in ATLAS dashboard
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established succesfully");
+});
 
 // start server
 app.listen(port, () => {
