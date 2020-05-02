582fe5c4c8dc115ea8da25bb629730b7262a2e61
oliver <oliverlsanz@gmail.com>
2020-05-02 12:24:56 +0200

    Add functionality to createUser and createExercise components

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,11 +1,136 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'
 
 export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+
+    // this code is needed so that the word "this" refers to the CreateExercise class in each method
+    // this way the methods can access this.setState
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    // create a state reflecting MongoDB exercise fields
+    // whenever the state changes, the component will update
+    this.state = {
+      username: '',
+      description: '',
+      duration: 0,
+      date: new Date(),
+      users: []
+    }
+  }
+
+  // this is a lifecicle method. That are methods that react will call at some point
+  componentDidMount() {
+    // we hardcode a user until we connect the frontend with the backend
+    this.setState({
+      users: ['tests user'],
+      username: 'test user'
+    })
+  }
+
+  onChangeUsername(e) {
+    // we never directly change state. Instead we use setState
+    // Only the specified field is changed
+    this.setState({
+      username: e.target.value  // target will be the textbox where the username is
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    });
+  }
+
+// The date will be a little different. We will use a library to make a calendar
+// appear and use that as input
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();  // prevents default html form submit behavior
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
+    }
+    console.log(exercise);
+
+    window.location = '/';
+  }
+
   render() {
     return (
       <div>
-       <p>You are on the Create Exercises component!</p>
-      </div>
+      <h3>Create New Exercise Log</h3>
+      <form onSubmit={this.onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}>
+              {
+                // we get the options from our users array
+                this.state.users.map(function(user) {
+                  return <option
+                    key={user}
+                    value={user}>{user}
+                    </option>;
+                })
+              }
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+              />
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+              type="text"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+              />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker
+              selected={this.state.date}
+              onChange={this.onChangeDate}
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
     )
   }
 }
