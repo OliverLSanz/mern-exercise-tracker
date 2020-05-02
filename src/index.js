28e6af7b3089c5416925b2e070c2b4b4d648df2e
oliver <oliverlsanz@gmail.com>
2020-05-02 10:27:48 +0200

    Frontend generated code reduced to Hello World

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import our custom css (Currently not in use)
+//import './index.css';
 
+// import the app
+import App from './App';
+
+// this code loads what App returns in the index.html "root" div
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
